ab786e595676138a5fa54b05f3f92a3880f27e1d
varshanth-c <155641278+varshanth-c@users.noreply.github.com>
2025-04-09 15:21:59 +0530

    yes

--- web-crud/src/main/java/org/pes/web_crud/BasicAPI.java
+++ web-crud/src/main/java/org/pes/web_crud/BasicAPI.java
@@ -1,8 +1,6 @@
 package org.pes.web_crud;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
