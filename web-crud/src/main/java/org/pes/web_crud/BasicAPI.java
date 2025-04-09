32d5183786049c8d9479cbc37833ef59f3f468b4
razzaksr <razzaksr@gmail.com>
2025-04-09 12:41:19 +0530

    Basic API for Get, Post, Param and Body

--- web-crud/src/main/java/org/pes/web_crud/BasicAPI.java
+++ web-crud/src/main/java/org/pes/web_crud/BasicAPI.java
@@ -1,6 +1,9 @@
 package org.pes.web_crud;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,19 @@ public class BasicAPI {
     @RequestMapping(value="/", method = RequestMethod.GET)
     public String greetings(){
         return "Welcome To DLithe";
+    }
+    // @GetMapping("/{height}/{weight}")
+    @RequestMapping(value="/{height}/{weight}", method = RequestMethod.GET)
+    public double calculateBMI(@PathVariable double height,@PathVariable double weight){
+        height = height/100;
+        return weight/(height*height);
+    }
+
+    // @PostMapping("/")
+    @RequestMapping(value="/", method = RequestMethod.POST)
+    public double BMIViaBody(@RequestBody double[] scales){
+        double height = scales[0];double weight=scales[1];
+        height = height/100;
+        return weight/(height*height);
     }
 }
